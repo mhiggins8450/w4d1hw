88e73fc817680ce51044dd3ed188ee81186563ed
mhiggins8450 <146852125+mhiggins8450@users.noreply.github.com>
2023-10-29 18:00:40 -0400

    created hamster class

--- w4d1hw.js
+++ w4d1hw.js
@@ -1,22 +1,19 @@
 class Hamster {
     constructor(name) {
-      this.owner = '';
+      this.owner = "";
       this.name = name;
       this.price = 15;
     }
   
     wheelRun() {
-      console.log('squeak squeak');
+      console.log("squeak squeak");
     }
   
     eatFood() {
-      console.log('nibble nibble');
+      console.log("nibble nibble");
     }
   
     getPrice() {
       return this.price;
     }
   }
-
-
-  
